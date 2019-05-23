671bea2681fa7b4c596c99323709e17d1d2a5bdd
DrewHans555 <drewhans555@gmail.com>
2019-05-22 19:14:42 -0500

    Fix formatting, add return-to-top btn on long pages

---
+++ assets/javascript/main.js
@@ -0,0 +1,24 @@
+/* --- Return-To-Top jQuery --- */
+
+// when page is scrolled
+$(window).scroll(function() {
+  // if page is scrolled 300px or more from top
+  if ($(this).scrollTop() >= 300) {
+    // fade in the arrow
+    $("#return-to-top").fadeIn(200);
+  } else {
+    // else fade out the arrow
+    $("#return-to-top").fadeOut(200);
+  }
+});
+
+// when return-to-top is clicked
+$("#return-to-top").click(function() {
+  // scroll to top of body
+  $("body,html").animate(
+    {
+      scrollTop: 0
+    },
+    500 // animation speed in milliseconds
+  );
+});
