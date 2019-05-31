e4dcc8c6eaf9ecfbf7012265bb54a2e3a1ab3c03
DrewHans555 <drewhans555@gmail.com>
2019-05-31 08:24:07 -0500

    Force cache update

--- sw.js
+++ sw.js
@@ -1,6 +1,6 @@
 /* --- service worker --- */
 
-const CACHENAME = "cache-v2019-05-31";
+const CACHENAME = "cache-2019-05-31";
 
 // never cache these resources, they change too frequently
 const URLBLACKLIST = [
@@ -8,6 +8,7 @@ const URLBLACKLIST = [
   "drewhans555.github.io/index.html",
   "drewhans555.github.io/manifest.json",
   "drewhans555.github.io/sw.js",
+  "drewhans555.github.io/assets/javascript/darkmode.js",
   "drewhans555.github.io/assets/javascript/main.js",
   "drewhans555.github.io/assets/javascript/register-sw.js",
   "drewhans555.github.io/assets/stylesheets/main.css",
