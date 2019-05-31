c0717e90bd0a5f041cfcbe2cbaaf24b157ec8147
DrewHans555 <drewhans555@gmail.com>
2019-05-31 11:04:24 -0500

    Refactor darkmode.js to be more readable

--- assets/javascript/darkmode.js
+++ assets/javascript/darkmode.js
@@ -2,35 +2,43 @@
 const supports_local_storage = "localStorage" in window;
 const dark_mode_setting = "apply-dark-mode";
 
+function disable_darkmode_stylesheet() {
+  document.getElementById("darkmode-stylesheet").disabled = true;
+}
+
+function enable_darkmode_stylesheet() {
+  document.getElementById("darkmode-stylesheet").disabled = false;
+}
+
+function toggle_darkmode() {
+  const apply_dark_mode = localStorage.getItem(dark_mode_setting);
+  if (apply_dark_mode === "false" || apply_dark_mode === null) {
+    localStorage.setItem(dark_mode_setting, "true");
+    enable_darkmode_stylesheet();
+  } else {
+    localStorage.setItem(dark_mode_setting, "false");
+    disable_darkmode_stylesheet();
+  }
+}
+
 /* --- on page load --- */
 if (supports_local_storage) {
   const apply_dark_mode = localStorage.getItem(dark_mode_setting);
   if (apply_dark_mode === "true") {
-    document.getElementById("darkmode-stylesheet").disabled = false;
+    enable_darkmode_stylesheet();
+  } else {
+    disable_darkmode_stylesheet();
   }
 } else {
-  document.getElementById("darkmode-stylesheet").disabled = true;
+  disable_darkmode_stylesheet();
   $("#dark-mode-toggle").hide();
 }
 
 $(document).ready(function() {
-
   /* --- on darkmode button press --- */
   $("#dark-mode-toggle").click(function() {
     if (supports_local_storage) {
-      const apply_dark_mode = localStorage.getItem(dark_mode_setting);
-
-      if (apply_dark_mode === "false" || apply_dark_mode === null) {
-        // turn dark mode on
-        document.getElementById("darkmode-stylesheet").disabled = false;
-        localStorage.setItem(dark_mode_setting, "true");
-      } else {
-        // turn dark mode off
-        document.getElementById("darkmode-stylesheet").disabled = true;
-        localStorage.setItem(dark_mode_setting, "false");
-      }
-
+      toggle_darkmode();
     }
   });
-
 });
