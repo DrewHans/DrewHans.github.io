001152db9f3654d174c3cd112bf449e1307a24d3
DrewHans555 <drewhans555@gmail.com>
2019-05-31 07:59:31 -0500

    Refactor javascript, revert darkmode anchor to button

--- assets/javascript/main.js
+++ assets/javascript/main.js
@@ -1,29 +1,34 @@
-/* --- last-update text --- */
-if($("#last-update").length) {
-    document.getElementById("last-update").innerHTML =
-        "&copy; Drew Hans, last update " + document.lastModified + "";
-}
+/* --- main.js --- */
+$(document).ready(function() {
 
-/* --- Return-To-Top button --- */
-// when page is scrolled
-$(window).scroll(function() {
-  // if page is scrolled 300px or more from top
-  if ($(this).scrollTop() >= 300) {
-    // fade in the arrow
-    $("#return-to-top").fadeIn(200);
-  } else {
-    // else fade out the arrow
-    $("#return-to-top").fadeOut(200);
+  /* --- last-update text --- */
+  if($("#last-update").length) {
+      document.getElementById("last-update").innerHTML =
+          "&copy; Drew Hans, last update " + document.lastModified + "";
   }
-});
 
-// when return-to-top is clicked
-$("#return-to-top").click(function() {
-  // scroll to top of body
-  $("body,html").animate(
-    {
-      scrollTop: 0
-    },
-    500 // animation speed in milliseconds
-  );
+  /* --- Return-To-Top button --- */
+  // when page is scrolled
+  $(window).scroll(function() {
+    // if page is scrolled 300px or more from top
+    if ($(this).scrollTop() >= 300) {
+      // fade in the arrow
+      $("#return-to-top").fadeIn(200);
+    } else {
+      // else fade out the arrow
+      $("#return-to-top").fadeOut(200);
+    }
+  });
+
+  // when return-to-top is clicked
+  $("#return-to-top").click(function() {
+    // scroll to top of body
+    $("body,html").animate(
+      {
+        scrollTop: 0
+      },
+      500 // animation speed in milliseconds
+    );
+  });
+
 });
